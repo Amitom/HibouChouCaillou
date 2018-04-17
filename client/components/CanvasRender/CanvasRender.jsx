f699025802a88bc11c27c29a021036f2bdeb2729
Amitom <thomas.cochet@gmail.com>
2018-04-17 19:52:25 -0400

    Add bootstrap 4

--- client/components/CanvasRender/CanvasRender.jsx
+++ client/components/CanvasRender/CanvasRender.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import { FormattedMessage } from 'react-intl';
 import React3 from 'react-three-renderer';
 import * as THREE from 'three';
 
 class CreationBox extends Component {
 
   static defaultProps = {
-    cameraDistance: 5,
+    cameraDistance: 2,
+    texture: {
+      wire: false,
+    },
   }
 
   constructor(props, context) {
@@ -20,6 +24,7 @@ class CreationBox extends Component {
         perspective: 0, // x, y, z
       },
       cameraPosition: new THREE.Vector3(0, 0, this.props.cameraDistance),
+      texture: props.texture,
     };
 
     this._onAnimate = () => {
@@ -50,12 +55,38 @@ class CreationBox extends Component {
   }
 
   onChangeCameraPerspective = (e) => {
-    this.angleToPosition(this.state.cameraAngles.y, e.target.value);
+    let angle = e.target.value;
+
+    if (angle < -90) {
+      angle = -90;
+    }
+    else if (angle > 90) {
+      angle = 90;
+    }
+
+    this.angleToPosition(this.state.cameraAngles.y, angle);
   };
 
   onChangeCameraYAxis = (e) => {
-    this.angleToPosition(e.target.value, this.state.cameraAngles.perspective);
+    let angle = e.target.value;
+    
+    if (angle < 0) {
+      angle = 0;
+    }
+    else if (angle > 360) {
+      angle = 360;
+    }
+
+    this.angleToPosition(angle, this.state.cameraAngles.perspective);
   };
+
+  onWireTextureChange = (e) => {
+    const texture = this.state.texture;
+    texture.wire = e.target.checked;
+    this.setState({
+      texture,
+    })
+  }
 
   render() {
     const width = window.innerWidth; // canvas width
@@ -63,12 +94,37 @@ class CreationBox extends Component {
 
     return (
       <section className="canvasrender">
-        <input id="cameraYAxis" type="range" min={0} max={360} value={this.state.cameraAngles.y} onChange={this.onChangeCameraYAxis} />
-        <input id="cameraPerspective" type="range" min={-90} max={90} value={this.state.cameraAngles.perspective} onChange={this.onChangeCameraPerspective} />
+        <div className="form-group row">
+          <label className="col-sm-2">
+            <FormattedMessage id="canvasrender.horizontalRotation" />
+          </label>
+          <div className="col-sm-10">
+            <input id="cameraYAxis" className="form-control-range" type="range" min={0} max={360} value={this.state.cameraAngles.y} onChange={this.onChangeCameraYAxis} />
+            <input type="number" min={0} max={360} value={this.state.cameraAngles.y} onChange={this.onChangeCameraYAxis} />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2">
+            <FormattedMessage id="canvasrender.verticalRotation" />
+          </label>
+          <div className="col-sm-10">
+            <input id="cameraPerspective" className="form-control-range" type="range" min={-90} max={90} value={this.state.cameraAngles.perspective} onChange={this.onChangeCameraPerspective} />
+            <input type="number" min={-90} max={90} value={this.state.cameraAngles.perspective} onChange={this.onChangeCameraPerspective} />
+           </div>
+         </div>
+        <div className="form-group row"> 
+          <label className="col-sm-2">
+            <FormattedMessage id="canvasrender.wireTexture" />
+          </label>
+          <div className="col-sm-10">
+            <input id="cameraPerspective" type="checkbox" value={this.state.texture.wire} onChange={this.onWireTextureChange} />
+          </div>
+        </div>
         <React3
           mainCamera="camera"
           width={width}
           height={height}
+          shadowMapEnabled
 
           onAnimate={this._onAnimate}
         >
@@ -91,13 +147,13 @@ class CreationBox extends Component {
             <spotLight
               color={0xffffff}
               intensity={1.5}
-              position={this.state.cameraPosition}
+              position={new THREE.Vector3(0, 0, this.props.cameraDistance)}
               lookAt={new THREE.Vector3(0, 0, 0)}
 
               castShadow
-              shadowCameraNear={200}
-              shadowCameraFar={10000}
-              shadowCameraFov={50}
+              shadowCameraNear={20}
+              shadowCameraFar={100}
+              shadowCameraFov={5}
 
               shadowBias={-0.00022}
 
@@ -105,7 +161,10 @@ class CreationBox extends Component {
               shadowMapHeight={2048}
             />
 
-            <mesh>
+            <mesh
+              castShadow
+              receiveShadow
+            >
               <boxGeometry
                 width={1}
                 height={1}
@@ -113,7 +172,7 @@ class CreationBox extends Component {
               />
               <meshBasicMaterial
                 color={0x009900}
-                wireframe={false}
+                wireframe={this.state.texture.wire}
               />
             </mesh>
           </scene>
--- client/i18n/messages/fr.js
+++ client/i18n/messages/fr.js
@@ -3,4 +3,8 @@ module.exports = {
   "footer.copyright": "© {year} Thomas Cochet - Tous droits réservés",
   "homepage.title": "Présentation",
   "creationpage.title": "Création",
+
+  "canvasrender.verticalRotation": "Rotation verticale",
+  "canvasrender.horizontalRotation": "Rotation horinzontale",
+  "canvasrender.wireTexture": "Texture filaire",
 }
